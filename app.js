bae7f7f28a9c0f12d8eb0224d450e0da5204d86b
MWMCKENNA <MWMCKENNA@GMAIL.COM>
2014-10-31 09:10:11 -0700

    one change

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
 app.use(session( {
-  
+  secret: '9870897432456587',
   name: 'grocery key',
   // this is in milliseconds
   maxage: 3600000
